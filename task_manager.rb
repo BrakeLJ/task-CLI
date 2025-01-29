79ef81a161bda2231a09e981cdfcb6fe4c30c482
Lucas Brake <lukejbrake@gmail.com>
2025-01-28 19:56:18 -0700

    feat: add list method to read tasks listed in the tasks.json file with a fall back if that file doesn't exist

--- task_manager.rb
+++ task_manager.rb
@@ -1,1 +1,23 @@
 #!/usr/bin/env ruby
+require "json"
+require "thor"
+
+TASKS_FILE = "tasks.json"
+
+class TaskManager < Thor
+  def initialize(*args)
+    super
+    @tasks = File.exist?(TASKS_FILE) ? JSON.parse(File.read(TASKS_FILE)) : []
+  end
+
+  desc "list", "List all tasks"
+  def list
+    if @tasks.empty?
+      puts "No tasks were found"
+    else
+      @tasks.each_with_index { |task, i| puts "#{i+1}. #{task}"}
+    end
+  end
+end
+
+TaskManager.start(ARGV)

6e69075a9b5c0b57d8ab2a3037648ead19899f08
Lucas Brake <lukejbrake@gmail.com>
2025-01-28 20:10:10 -0700

    feat: impliment add methos as well as a private method to save the task list

--- task_manager.rb
+++ task_manager.rb
@@ -18,6 +18,21 @@ class TaskManager < Thor
       @tasks.each_with_index { |task, i| puts "#{i+1}. #{task}"}
     end
   end
+
+  desc "add", "Add a task to the list"
+  def add(task)
+    @tasks << task 
+    save_tasks
+    puts "#{task} added to the list."
+  end
+
+  private 
+
+  def save_tasks 
+    File.write(TASKS_FILE, JSON.pretty_generate(@tasks))
+  end
 end
+
+
 
 TaskManager.start(ARGV)

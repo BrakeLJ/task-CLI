bc0e2050d1d7ade490efec3da582501acf90d825
Lucas Brake <lukejbrake@gmail.com>
2025-01-29 21:49:46 -0700

    feat: add logic to parse through csv and store each row as a hash in the products array

--- csv_parser.rb
+++ csv_parser.rb
@@ -9,6 +9,14 @@ class CSVParser
 
   def parse_csv 
     # load the file and read each row adding it as a hash to the products array
+
+    CSV.foreach(FILE_NAME, headers: true) do | row |
+      @products << {
+        name: row["name"],
+        price: row["price"].to_f,
+        quantity: row["quantity"].to_i
+    }.transform_keys(&:to_sym)
+    end
   end
 
   def products_value
@@ -18,5 +26,6 @@ class CSVParser
 
   def run 
     # run the parse_csv and products_value methods
+    parse_csv
   end
 end

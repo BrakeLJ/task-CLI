0ad1e680f61ea8ecf957f42169d5563f5f208653
Lucas Brake <lukejbrake@gmail.com>
2025-01-29 21:36:49 -0700

    feat: add code skeleton for desired methods

--- csv_parser.rb
+++ csv_parser.rb
@@ -1,1 +1,22 @@
 require 'csv'
+
+class CSVParser
+  FILE_NAME = 'products.csv'
+
+  def initialize 
+    @products = []
+  end
+
+  def parse_csv 
+    # load the file and read each row adding it as a hash to the products array
+  end
+
+  def products_value
+    # display each product and total value in inventory by multiplying the value by the quantity
+    # display a total value for all inventory in stock
+  end
+
+  def run 
+    # run the parse_csv and products_value methods
+  end
+end
